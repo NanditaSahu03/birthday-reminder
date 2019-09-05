53ac98260892fae5a8d15303cecf1fbac7ef301e
nandita sahu <Savegirlchild@users.noreply.github.com>
2019-09-05 17:52:30 +0530

    Add files via upload

---
+++ birthday_reminder.py
@@ -0,0 +1,29 @@
+
+import time 
+import os 
+
+# Birthday file is the one in which the actual birthdays 
+# and dates are present. This file can be 
+# manually edited or can be automated. 
+# For simplicity, we will edit it manually. 
+# Birthdays should be written in this file in 
+# the format: "MonthDay Name Surname" (Without Quotes) 
+
+birthdayFile="birthday.txt"
+
+def checkTodaysBirthdays(): 
+	fileName = open(birthdayFile,'r') 
+	today = time.strftime('%m%d') 
+	flag = 0
+	for line in fileName: 
+		if today in line: 
+			line = line.split(' ') 
+			flag =1
+			# line[1] contains Name and line[2] contains Surname 
+			os.system('notify-send "Birthdays Today: ' + line[1] 
+			+ ' ' + line[2] + '"') 
+	if flag == 0: 
+			os.system('notify-send "No Birthdays Today!"') 
+
+ 
+checkTodaysBirthdays() 
